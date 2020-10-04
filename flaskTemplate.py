8d01a5f25cd82ae1154ebb8128478cf0b5b7c653
gangulyarin <gangulyarin@users.noreply.github.com>
2020-10-04 11:45:08 +0530

    Deployed to IBM Cloud

--- flaskTemplate.py
+++ flaskTemplate.py
@@ -1,13 +1,13 @@
 import flask
 
-app = flask.Flask(__name__)
+app = flask.Flask(__name__, static_url_path='')
 app.config["DEBUG"] = True
 
 
 @app.route('/', methods=['GET'])
-def home():
+def root():
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
 #app.run()
 if __name__ == '__main__':
-    app.run(host="localhost", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
